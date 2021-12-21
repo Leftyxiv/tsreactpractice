d41858cc58b00422c498825081650e29ca184546
leftyxiv <manny@aledoux.net>
2021-12-21 13:57:07 -0500

    modified some settings

---
+++ modules/src/index.js
@@ -0,0 +1,4 @@
+var multiply = function (a, b) { return a * b; };
+var a = 2;
+var b = 2;
+console.log(multiply(a, b));
---
+++ modules/src/index.ts
@@ -0,0 +1,6 @@
+const multiply = (a: number, b: number) => a * b;
+
+const a = 2;
+const b = 2;
+
+console.log(`${a} * ${b} = ${multiply(a, b)}`);
